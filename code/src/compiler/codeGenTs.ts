60a01b1d3676eeab953fe9a2b6b4f1b31ff97c73
Yifan Wu <yifan1030@gmail.com>
2018-11-07 10:34:20 -0800

    cleaned up typed TS generation

--- code/src/compiler/codeGenTs.ts
+++ code/src/compiler/codeGenTs.ts
@@ -1,7 +1,9 @@
+import * as fs from "fs";
 import * as stream from "stream";
 import { DielIr, RelationIr, DerivedRelationIr, DataType } from "../parser/dielTypes";
 import { RelationTs, RelationType } from "../lib/dielUtils";
 import * as fmt from "typescript-formatter";
+import { LogInternalError } from "../util/messages";
 
 
 function dataTypeToTypeScript(t: DataType) {
@@ -23,7 +25,7 @@ function generateInputDelcaration(i: RelationIr) {
 
 function generateInput(i: RelationIr) {
     return `${i.name}: (val: {${i.columns.map(c => `${c.name}: ${dataTypeToTypeScript(c.type)}`).join(", ")}}) => {
-      this.input.${i.name}(val);
+      this.input.${i.name}.run(val);
     }`;
 }
 
@@ -34,10 +36,11 @@ function generateView(v: DerivedRelationIr) {
   const returnStmt = v.isSingle
     ? `if (r.length > 1) throw new Error (${v.name} should have single value);
         return r[0];`
-    : `return r`;
+    : `return r;`;
 
   return `${v.name}: () => {
-    this.view.${v.name}.bind({});
+    const s = this.view.${v.name};
+    s.bind({});
     let r = [];
     while (s.step()) {
       r.push(s.getAsObject());
@@ -53,11 +56,11 @@ function generateDependencies(ir: DielIr) {
   // for each output, look at its inputs
   const inputDependency = new Map<string, string[]>();
   ir.outputs.map(o => {
-    [o.selectBody.fromRelation, ...o.selectBody.joinRelations].map(r => {
-      if (inputDependency.has(r)) {
-        inputDependency.get(r).push(o.name);
+    o.selectBody.relations.map(r => {
+      if (inputDependency.has(r.name)) {
+        inputDependency.get(r.name).push(o.name);
       } else {
-        inputDependency.set(r, [o.name]);
+        inputDependency.set(r.name, [o.name]);
       }
     });
   });
@@ -67,36 +70,39 @@ function generateDependencies(ir: DielIr) {
 function generateDepMapString(m: Map<string, string[]>) {
   let strings: string[] = [];
   for (let [key, value] of m.entries()) {
-    strings.push(`[${key}, [${value.join(", ")}]]`);
+    strings.push(`["${key}", [${value.map(v => `"${v}"`).join(", ")}]]`);
   }
   return `[${strings.join(", ")}]`;
 }
 
-function generateStatementDeclarations(ir: DielIr) {
-  const genDeclaration = (a: (RelationIr | DerivedRelationIr)[]) => a.map(o => `${o.name}: Statement;`).join("\n");
-  return `
-    private input = {${genDeclaration(ir.inputs)}};
-    private output = {${genDeclaration(ir.outputs)}};
-    private view = {${genDeclaration(ir.views)}};
-    private dynamicTable = {${genDeclaration(ir.tables.filter(o => o.isDynamic))}};
-  `;
-}
 function generateStatements(ir: DielIr) {
-  const genPrep = (a: RelationTs[]) => a.map(o => `${o.name}: this.db.prepare(\`${o.query}\`);`).join("\n");
-  const inputArray = createInputTs(ir.inputs);
-  const outputArray = createOutputTs(ir.outputs);
-  const viewArray = createViewsTs(ir.views);
-  const dynamicTable = createDynamicTableTs(ir.tables);
-  return `
-    this.input = {${genPrep(inputArray)}};
-    this.output = {${genPrep(outputArray)}};
-    this.view = {${genPrep(viewArray)}};
-    this.dynamicTable = {${genPrep(dynamicTable)}};
-  `;
+  const genPrep = (a: RelationTs[]) => a.map(o => `${o.name}: this.db.prepare(\`${o.query}\`)`).join(",\n");
+  const genDeclaration = (a: (RelationIr | DerivedRelationIr)[]) => a.map(o => `${o.name}: Statement,`).join("\n");
+  console.log("genDeclaration of inputs", genDeclaration(ir.inputs));
+  let assignment: string[] = [];
+  let declaration: string[] = [];
+  if (ir.inputs.length > 0) {
+    declaration.push(`private input: {${genDeclaration(ir.inputs)}};`);
+    assignment.push(`this.input = {${genPrep(createInputTs(ir.inputs))}};`);
+  }
+  const allViews = ir.views.concat(ir.outputs);
+  if (allViews.length > 0) {
+    declaration.push(`private view: {${genDeclaration(allViews)}};`);
+    assignment.push(`this.view = {${genPrep(createOutputTs(allViews))}};`);
+  }
+  const dynamicTables = ir.tables.filter(t => t.isDynamic);
+  if (dynamicTables.length > 0) {
+    declaration.push(`private dynamicTable: {${genDeclaration(dynamicTables)}};`);
+    assignment.push(`this.dynamicTable = {${genPrep(createDynamicTableTs(dynamicTables))}};`);
+  }
+  return {
+    declaration,
+    assignment
+  };
 }
 
 function generateBindOutputDeclaration(o: DerivedRelationIr) {
-  return `${o.name}: (f: OutputBoundFunc) => void;`;
+  return `${o.name}: (f: OutputBoundFunc) => void`;
 }
 
 function generateGetViewDeclaration(o: DerivedRelationIr) {
@@ -104,61 +110,72 @@ function generateGetViewDeclaration(o: DerivedRelationIr) {
   return `${o.name}: () => {${o.columns.map(c => `${c.name}: ${dataTypeToTypeScript(c.type)}`)}}[]`;
 }
 
-function generateApiDeclaration(ir: DielIr) {
-  return `
-    public NewInput: {${ir.inputs.map(generateInputDelcaration)}};
-    public TableInput: {${ir.tables.filter(t => t.isDynamic).map(generateInputDelcaration)}};
-    public BindOutput: {${ir.outputs.map(generateBindOutputDeclaration)};
-    public GetView: {${ir.views.filter(v => v.isPublic).concat(ir.outputs).map(generateGetViewDeclaration)}};
-  `;
+function generateBindOutput(o: DerivedRelationIr) {
+  return `${o.name}: (f: OutputBoundFunc) => {
+    this.sideEffects["${o.name}"] = () => {
+      const r = this.GetView.${o.name}();
+      f(r);
+    };
+  }`;
 }
 
 function generateApi(ir: DielIr) {
-  return `
-    this.NewInput = {${ir.inputs.map(generateInput)}};
-    this.TableInput = {${ir.tables.filter(t => t.isDynamic).map(generateInput)}};
-    this.BindOutput = {
-        ${ir.outputs.map(o => `${o.name}: (f: OutputBoundFunc) => {
-          this.sideEffects["${o.name}"] = () => {
-            const r = this.GetView.${o.name}();
-            f(r);
-          };
-        }`)}
-      }
-    this.GetView = {
-        ${ir.views.filter(v => v.isPublic).concat(ir.outputs).map(generateView)}
-      }
-    }`;
+  let assignment: string[] = [];
+  let declaration: string[] = [];
+  if (ir.inputs.length > 0) {
+    declaration.push(`public NewInput: {${ir.inputs.map(generateInputDelcaration).join(",\n")}};`);
+    assignment.push(`this.NewInput = {${ir.inputs.map(generateInput)}};`);
+  }
+  const pubTables = ir.tables.filter(t => t.isDynamic);
+  if (pubTables.length > 0) {
+    declaration.push(`public TableInput: {${pubTables.map(generateInputDelcaration).join(",\n")}};`);
+    assignment.push(`this.TableInput = {${pubTables.map(generateInput)}};`);
+  }
+  if (ir.outputs.length > 0) {
+    declaration.push(`public BindOutput: {${ir.outputs.map(generateBindOutputDeclaration).join(",\n")}};`);
+    assignment.push(`this.BindOutput = {${ir.outputs.map(generateBindOutput)}};`);
+  }
+  const pubViews = ir.views.filter(v => v.isPublic).concat(ir.outputs);
+  if (pubViews.length > 0) {
+    declaration.push(`public GetView: {${pubViews.map(generateGetViewDeclaration).join("\n")}};`);
+    assignment.push(`this.GetView = {${pubViews.map(generateView)}};`);
+  }
+  return {
+    declaration,
+    assignment
+  };
 }
 
 function generateDiel(ir: DielIr) {
   const dependencies = generateDepMapString(generateDependencies(ir));
-
+  const statements = generateStatements(ir);
+  const apis = generateApi(ir);
   return `
-import { Statement } from "sql.js";
-import { loadDbHelper, OutputBoundFunc } from "diel";
+import { Database, Statement } from "sql.js";
+import { loadDbHelper, OutputBoundFunc, LogInfo } from "diel";
 
 export class Diel {
-  dbPath: string;
-  sideEffects: {{[index: string]: () => void};
-  ${generateStatementDeclarations(ir)}
-  ${generateApiDeclaration(ir)}
+  private db: Database;
+  private dbPath: string;
+  private sideEffects: {[index: string]: () => void};
+  ${statements.declaration.join("\n")}
+  ${apis.declaration.join("\n")}
   constructor(dbPath: string) {
     LogInfo("DIEL initializing");
     this.dbPath = dbPath;
     this.sideEffects = {};
   }
   public async Setup() {
-    await this.LoadDb(path);
-    ${generateStatements(ir)}
-    ${generateApi(ir)}
+    await this.LoadDb(this.dbPath);
+    ${statements.assignment.join("\n")}
+    ${apis.assignment.join("\n")}
   }
   public async LoadDb(file: string) {
     loadDbHelper(this.db, file, this.tick);
   }
   private tick() {
     const sideEffects = this.sideEffects;
-    const dependencies = new Map<string, string[]>(${dependencies});;
+    const dependencies = new Map<string, string[]>(${dependencies});
     return (input: string) => {
       const updates = dependencies.get(input);
       updates.map(u => sideEffects[u]());
@@ -179,7 +196,7 @@ function createInputTs(ins: RelationIr[]): RelationTs[] {
 }
 
 function createDynamicTableTs(tables: RelationIr[]): RelationTs[] {
-  return tables.filter(t => t.isDynamic).map(t => ({
+  return tables.map(t => ({
     // relationType: RelationType.Table,
     name: t.name,
     query: `insert into ${t.name} (${t.columns.map(c => c.name).join(", ")})`
@@ -204,6 +221,7 @@ function createViewsTs(views: DerivedRelationIr[]): RelationTs[]  {
 
 export async function genTs(ir: DielIr) {
   const rawText = generateDiel(ir);
+  fs.writeFileSync("tmpUnformatted.ts", rawText);
   let input = new stream.Readable();
   input.push(rawText);
   input.push(null);
@@ -222,5 +240,9 @@ export async function genTs(ir: DielIr) {
     tsfmtFile: null,
   };
   const t = await fmt.processStream("tmp.ts", input, formatConfig);
-  return t;
+  if (t.error) {
+    LogInternalError(`Formatting TypeScript Failed with error ${t.message}`);
+  }
+  // console.log("Pretty print TS", t.dest);
+  return t.dest;
 }
--- code/src/tests/compilerTest.ts
+++ code/src/tests/compilerTest.ts
@@ -7,14 +7,14 @@ import { LogStandout } from "../util/messages";
 // import { sanityIr } from "../compiler/errorChecking";
 import { genFiles } from "../compiler/fileGen";
 
-function testQuery(q: string, createFile: boolean) {
+async function testQuery(q: string, createFile: boolean) {
   let ir = getIR(q);
   console.log(`Generated IR:\n${JSON.stringify(ir, null, 2)}\n`);
   // modify IR based on crossfiler
   // console.log(`Sanity checking:\n${sanityIr(ir)}`);
   // console.log(`Generated TS:\n${genTs(ir)}`);
-
-  fs.writeFileSync("tmp.ts", genTs(ir));
+  const doc = await genTs(ir);
+  fs.writeFileSync("tmp.ts", doc);
   // console.log(`Generated SQL:\n${genSql(ir).join("\n\n")}`);
   if (createFile) {
     // TODO: maybe clean some files out before running each tme
@@ -23,19 +23,20 @@ function testQuery(q: string, createFile: boolean) {
   return;
 }
 
-function main() {
+async function main() {
   console.log("starting tests");
   // ["tests", "testsComplex", "testEnd2End",
   //  "testOperators", "testXFilter", "testTemplate",
   //  "testTmp", "testsConstraint"].map(fn => {
 
-  ["testEnd2End"].map(fn => {
+  const r = ["testEnd2End"].map(fn => {
     const tests = fs.readFileSync(`./src/tests/${fn}.sql`, "utf8").split(/-- TEST: \w+\n/);
     tests.filter(t => t.length > 0).map(t => {
       LogStandout(`Running\n${t}`);
       testQuery(t, false);
     });
   });
+  Promise.all(r).then(() => true);
 }
 
 main();
