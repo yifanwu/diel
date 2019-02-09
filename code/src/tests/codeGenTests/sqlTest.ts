e0a2111b1f1b72eeb13459f07ad9b2978fcb06e5
yifanwu <yifan1030@gmail.com>
2019-02-08 21:17:25 -0800

    added a few other test cases

--- code/src/tests/codeGenTests/sqlTest.ts
+++ code/src/tests/codeGenTests/sqlTest.ts
@@ -1,7 +1,7 @@
 import { generateSelectionUnit } from "../../compiler/codegen/codeGenSql";
 import { GenerateUnitTestErrorLogger } from "../../lib/messages";
-import { SelectionUnit } from "../../parser/sqlAstTypes";
-import { ExprType } from "../../parser/exprAstTypes";
+import { SelectionUnit, JoinType, AstType } from "../../parser/sqlAstTypes";
+import { ExprType, FunctionType } from "../../parser/exprAstTypes";
 import { DataType } from "../../parser/dielAstTypes";
 
 /**
@@ -22,14 +22,52 @@ export function codeGenBasicSQLTest() {
       relationName: "t1"
     },
     alias: "a"
-  }];
+  },
+  {
+    expr: {
+      exprType: ExprType.Column,
+      dataType: DataType.TBD,
+      columnName: "",
+      hasStar: true,
+      relationName: "t2"
+    }
+  }
+  ];
+  const predicate = {
+    exprType: ExprType.Func,
+    dataType: DataType.Boolean,
+    functionType: FunctionType.Compare,
+    functionReference: "=",
+    args: [
+      {
+        exprType: ExprType.Column,
+        dataType: DataType.TBD,
+        columnName: "aId",
+        hasStar: false,
+        relationName: "t1"
+      },
+      {
+        exprType: ExprType.Column,
+        dataType: DataType.TBD,
+        columnName: "aId",
+        hasStar: false,
+        relationName: "t2"
+      }
+    ]
+  };
   const ast: SelectionUnit = {
     derivedColumnSelections: columns,
     columnSelections: columns,
-    baseRelation: {relationName: "t1"}
+    baseRelation: {relationName: "t1"},
+    joinClauses: [{
+      astType: AstType.Join,
+      joinType: JoinType.LeftOuter,
+      relation: {relationName: "t2"},
+      predicate
+    }]
   };
   const sGen = generateSelectionUnit(ast);
-  const refSolution = `select t1.aId as a from t1`;
+  const refSolution = `select t1.aId as a, t2.* from t1 LEFT OUTER join t2 on t1.aId = t2.aId`;
   const logger = GenerateUnitTestErrorLogger("codeGenBasicSQLTest", refSolution);
   if (strip(sGen) !== strip(refSolution)) {
     logger(`Generated SQL is wrong. Expected ${refSolution}, but got ${sGen} instead`);
