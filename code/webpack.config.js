8c2534d376531b2ab5724eea5b6b885a628d24c4
yifanwu <yifan1030@gmail.com>
2019-02-18 12:51:47 -0800

    missing lib code

---
+++ code/webpack.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    entry: "./src/notebook/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/UI-dist",
+        publicPath: "/UI-dist",
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {loader: "awesome-typescript-loader" }
+                ]
+            },
+            // {
+            //     test: /\.js$/,
+            //     enforce: "pre",
+            //     loader: "source-map-loader",
+            //     exclude: "/node_modules/"
+            // },
+            // { 
+            //     test: /\.tsx?$/,
+            //     enforce: "pre",
+            //     loader: 'tslint-loader'
+            // }
+        ],
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "sql.js": "SQL",
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "d3": "d3",
+    },
+    
+    target: 'web',
+    devServer: {
+        inline: true
+    },
+};
