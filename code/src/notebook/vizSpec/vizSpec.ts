61572d7e118351908cd744345b2237ccbf2ddc1d
Lauren Go <go.lauren@berkeley.edu>
2019-03-14 11:21:50 -0700

    hi

--- code/src/notebook/vizSpec/vizSpec.ts
+++ code/src/notebook/vizSpec/vizSpec.ts
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import { DerivedRelation } from "../../parser/dielAstTypes";
 import DielRuntime from "../../runtime/DielRuntime";
 import { ChartType, RelationObject } from "../../runtime/runtimeTypes";
+=======
+import { DerivedRelation, DataType, CompositeSelectionUnit, SelectionUnit, ColumnSelection, SetOperator } from "../../parser/dielAstTypes";
+import DielRuntime from "../../runtime/DielRuntime";
+import { ChartType } from "../../runtime/runtimeTypes";
+import { ExprType, ExprFunAst, FunctionType } from "../../parser/exprAstTypes";
+import { generateSelectionUnit, generateSqlViews } from "../../compiler/codegen/codeGenSql";
+import { RelationQuery, SqlRelationType } from "../../compiler/codegen/createSqlIr";
+>>>>>>> hi
 
 export interface VizLayout {
   chartHeight: number;
@@ -107,7 +116,7 @@ export function generateVizSpecForSingleQuery(rt: DielRuntime, q: DerivedRelatio
   let columnSelections: ColumnSelection[] = selection.relation.columnSelections //
   
   let spec : VizSpec = null
-  
+  let query_copy = JSON.parse(JSON.stringify(q));
   let timeColumns: ColumnSelection[] = []
   let numericColumns: ColumnSelection[] = []
   let uselessColumns: ColumnSelection[] = []
@@ -127,18 +136,102 @@ export function generateVizSpecForSingleQuery(rt: DielRuntime, q: DerivedRelatio
     
     var combined = timeColumns.concat(numericColumns);
     var columnName = (combined[0].alias) ? combined[0].alias : "column";
-
-    
-    
-    
-    const r = rt.db.exec(`select count() from ${q.name} group by ${columnName}`);
+    combined[0].alias = columnName;
+    let count = {
+      functionType: FunctionType.Custom,
+      functionReference: "COUNT",
+      args: []
+    } as ExprFunAst;
+    var uniqueValues : SelectionUnit = {
+      columnSelections: [{expr: count}],
+      baseRelation: {relationName: q.name},
+      groupByClause: {
+        selections: [combined[0].expr]
+      }
+    };    
+    const r = rt.db.exec(generateSelectionUnit(uniqueValues));
+    var n = r.values.length;
+    if (n > 10) {
+      let c = combined[0];
+      let count = {
+        functionType: FunctionType.BuiltIn,
+        functionReference: "COUNT",
+        args: []
+      } as ExprFunAst;
+      let max = {
+        functionType: FunctionType.BuiltIn,
+        functionReference: "MAX",
+        args: [c.expr]
+      } as ExprFunAst;
+      let min = {
+        functionType: FunctionType.BuiltIn,
+        functionReference: "MIN",
+        args: [c.expr]
+      } as ExprFunAst;
+  
+      let argstats: RelationQuery = {
+        name: "argstats",
+        sqlRelationType: SqlRelationType.View,
+        query: [
+          {
+            op: SetOperator.NA,
+            relation:{
+              columnSelections: [{expr: max, alias: "max"}, {expr: min, alias: "min"}],
+              baseRelation: {relationName: q.name},
+            }
+          }
+        ],
+      };
+      rt.db.exec(generateSqlViews(argstats));
+      rt.db.exec(`create view bins as select round(10 * ${columnName} / (argstats.max - argstats.min)) as bin, ${columnName} from ${q.name}`)
+      query_copy.allDerivedSelections[0] = {
+        columnSelections: [
+          {expr: count}, {expr: "bin"}],
+        baseRelation: {relationName: "bins"},
+        groupByClause: { selections: [{exprType: ExprType.Column, dataType: DataType.Number, columnName: "bin", hasStar: false}]
+        }
+      }
+      spec = {
+        chartType: ChartType.BarChart,
+        modifiedQuery: query_copy,
+        xAxisColumn: columnName,
+        yAxisColumn: "quantity"
+      }
+    } else {
+      query_copy.allDerivedSelections[0] = {
+        columnSelections: [
+          {expr: count}, combined[0]],
+        baseRelation: {relationName: q.name},
+        groupByClause: { selections: [combined[0].expr] }
+      };
+      spec = {
+        chartType: ChartType.BarChart,
+        modifiedQuery: query_copy,
+        xAxisColumn: columnName,
+        yAxisColumn: "quantity"
+      }
+    }
   } else if (timeColumns.length + numericColumns.length == 2)
    {
-
-  } else {
-    return spec
+    if (!(timeColumns.length==0)) {
+      query_copy.allDerivedSelections[0].columnSelections = [timeColumns[0], numericColumns[0]];
+      spec = {
+        chartType: ChartType.LineChart,
+        modifiedQuery: query_copy,
+        xAxisColumn: (timeColumns[0].alias) ?  timeColumns[0].alias : "time",
+        yAxisColumn: (numericColumns[0].alias) ? numericColumns[0].alias : "attribute"
+      }
+    } else {
+      query_copy.allDerivedSelections[0].columnSelections = [numericColumns[0], numericColumns[1]];
+      spec = {
+        chartType: ChartType.Scatter,
+        modifiedQuery: query_copy,
+        xAxisColumn: (timeColumns[0].alias) ?  timeColumns[0].alias : "attribute1",
+        yAxisColumn: (numericColumns[0].alias) ? numericColumns[0].alias : "attribute2"
+      }
+    }
   }
-  const r = rt.db.exec(`select count() from totalWorkerClickEvents`);
-  console.log("%c TESTING", "color: red");
+  // const r = rt.db.exec(`select count() from totalWorkerClickEvents`);
+  // console.log("%c TESTING", "color: red");
   return spec;
 }
