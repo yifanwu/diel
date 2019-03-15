fffdb3b324ac0f7e87a26d5abd7f1a4ddfbaa710
Lucie Choi <lucie0407@berkeley.edu>
2019-03-15 03:25:29 -0700

    changing islatest ast done

--- code/src/compiler/passes/syntaxSugar.ts
+++ code/src/compiler/passes/syntaxSugar.ts
@@ -1,5 +1,11 @@
-import { DerivedRelation, SelectionUnit, DielAst } from "../../parser/dielAstTypes";
-
+import { DerivedRelation, SelectionUnit, DielAst, AstType } from "../../parser/dielAstTypes";
+import {ReportDielUserError} from "../../lib/messages";
+import { RelationReference } from "../../../dist/parser/sqlAstTypes";
+import { RelationSelection, CompositeSelectionUnit, SetOperator, OrderByAst, ColumnSelection, Order, DataType } from "../../../src/parser/dielAstTypes";
+import {ExprAst, ExprType} from "../../../src/parser/exprAstTypes";
+import {generateSelectionUnit} from "../../../src/compiler/codegen/codeGenSql";
+import { basename } from "path";
+import { CompositeSelection } from "../../../dist/parser/dielAstTypes";
 // implements the transformation for LATEST
 
 // should reference the implementation for `applyTemplates` in `applyTemplate.ts`.
@@ -26,5 +32,89 @@ export function applyLatestToAst(ast: DielAst): void {
  * @param relation
  */
 export function applyLatestToSelectionUnit(relation: SelectionUnit): void {
+    // console.log(relation.baseRelation.subquery.compositeSelections[0].relation.orderByClause);
+
+    if (relation.baseRelation.isLatest) {
+        if (relation.baseRelation.subquery !== undefined) {
+            // report error
+            return ReportDielUserError("Latest should be used with a simple named relation");
+        }
+        var relationName = relation.baseRelation.relationName;
+        // console.log("true");
+
+    var selection = {
+        op: SetOperator.NA,
+        relation : {
+            isDistinct: false,
+
+            columnSelections : [{
+                alias: null,
+                expr: {
+                    exprType: ExprType.Column,
+                    dataType: DataType.TBD,
+                    hasStar: true
+                } as ExprAst
+            }] as ColumnSelection[],
+
+            baseRelation: {
+                alias: null,
+                isLatest: false,
+                relationName: relationName
+            } as RelationReference,
+
+            orderByClause : [{
+                selection: {
+                    exprType: ExprType.Column,
+                    dataType: DataType.TBD,
+                    hasStar: false,
+                    columnName:  "timestep"
+                } as ExprAst,
+                order: Order.DESC
+            } as OrderByAst] as OrderByAst[],
+
+            limitClause : {
+                exprType: ExprType.Val,
+                dataType: DataType.Number,
+                value: 1
+            } as ExprAst
+        } as SelectionUnit
+    } as CompositeSelectionUnit;
+
+    // changing base relation in-place
+    relation.baseRelation.isLatest = false;
+    relation.baseRelation.subquery = {
+        astType: AstType.RelationSelection,
+        compositeSelections: [selection]
+    } as RelationSelection;
+    }
+
+    var q = generateSelectionUnit(relation);
+    console.log(q);
 
 }
+
+
+
+
+// { isDistinct: false,
+//     columnSelections: [ { alias: null, expr: [Object] } ],
+//     baseRelation: { alias: null, isLatest: false, relationName: 't1' },
+//     joinClauses: [],
+//     whereClause: null,
+//     groupByClause: null,
+//     orderByClause: [ { selection: [Object], order: 'DESC' } ],
+//     limitClause: { exprType: 'Val', dataType: 'Number', value: 1 } }
+
+
+
+// { isDistinct: false,
+//     columnSelections: [ { alias: null, expr: [Object] } ],
+//     baseRelation:
+//      { alias: null,
+//        subquery:
+//         { astType: 'RelationSelection', compositeSelections: [Array] } },
+//     joinClauses: [],
+//     whereClause: null,
+//     groupByClause: null,
+//     orderByClause: null,
+//     limitClause: null }
