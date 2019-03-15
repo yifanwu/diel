bd02403c8bf36ec16e35f442b6bcb79ec6db649a
yifanwu <yifan1030@gmail.com>
2019-03-14 23:44:25 -0700

    wip making pitchfork more fun, also made socket connection not default so its easier to set up (without the remote running)

--- code/src/notebook/components/PageContainer.tsx
+++ code/src/notebook/components/PageContainer.tsx
@@ -7,6 +7,7 @@ import ScoreZoomBarChart from "./examples/ScoreZoomBarChart";
 import PitchFork from "./examples/PitchFork";
 import Flights from "./examples/Flights";
 import VizSpecDemo from "./examples/VizSpecDemo";
+import { DEMO_WITH_SOCKET, DEMO_WITH_SMALL_WEBWORKER, DEMO_WITH_LARGE_WEBWORKER, DEMO_WITH_WEBWORKER } from "../../compiler/config";
 
 const Notebook = () => (<div>
     <ExistingRelationsPad/>
@@ -32,20 +33,24 @@ export const PageContainer = () => (<Router>
         Zoomable Bar Chart
       </Link>
       &nbsp;
-      <Link to="pitchfork">
-        Pitch Fork (Remote)
-      </Link>
-      <Link to="flights">
+      {DEMO_WITH_SOCKET
+        ? <Link to="pitchfork">
+          Pitch Fork (Remote)
+        </Link>
+      : null}
+      {DEMO_WITH_SMALL_WEBWORKER || DEMO_WITH_LARGE_WEBWORKER
+      ? <Link to="flights">
         Flights (Remote)
       </Link>
+      : null}
     </div>
     <div id="main">
       <Switch>
         <Route exact path="/counter" component={Counter}/>
         <Route path="/notebook" component={Notebook}/>
         <Route path="/scoreZoom" component={ScoreZoomBarChart}/>
-        <Route path="/pitchfork" component={PitchFork} />
-        <Route path="/" component={Flights} />
+        {DEMO_WITH_SOCKET ? <Route path="/pitchfork" component={PitchFork} /> : null}
+        {DEMO_WITH_WEBWORKER ? <Route path="/" component={Flights} /> : null}
         <Route path="/inferViz" component={VizSpecDemo}/>
       </Switch>
     </div>
