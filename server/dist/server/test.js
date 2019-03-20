bd79cde23d833c724a9bb32543cc04a4b8573f69
Lucie Choi <lucie0407@berkeley.edu>
2019-03-20 11:03:50 -0700

    changed package.json back

--- server/dist/server/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sqlite = require("better-sqlite3");
-const dbFile = "./sample-data/pitchfork.sqlite";
-const db = new sqlite(dbFile);
-const stmt = db.prepare("SELECT sql, name table_name FROM sqlite_master WHERE type='table' and sql not null");
-const r = stmt.all();
-console.log(JSON.stringify(r));
-//# sourceMappingURL=test.js.map
