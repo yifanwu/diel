9343069b053692611dec7382e3f8e41fd798e130
yifanwu <yifan1030@gmail.com>
2019-06-19 11:38:11 -0700

    additional helper method and cleanup deps from wasp atempt

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { WorkerConfig, SocketConfig, DbSetupConfig } from "./runtime/DbEngine";
 import { DbType, RelationObject, RecordObject } from "./runtime/runtimeTypes";
 import { DerivedRelation, RelationSelection, RelationNameType, CompositeSelection, ColumnNameType, OriginalRelation, HasDefault } from "./parser/dielAstTypes";
 import { GetAllStaticOriginalTables } from "./compiler/DielAstGetters";
+import { GenerateStrFromDielDerivedRelation } from "./compiler/codegen/codeGenSql";
 
 export {
   DielRuntime,
+  GenerateStrFromDielDerivedRelation,
   // the following are types that need to be exposed
   CompositeSelection,
   RelationSelection,
